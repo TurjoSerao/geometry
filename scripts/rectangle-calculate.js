2529cd24d89ed02977396e1f3156bcf6110fac65
devserao <seraotourjo@gmail.com>
2026-05-14 12:56:52 +0600

    first commit

---
+++ scripts/rectangle-calculate.js
@@ -0,0 +1,22 @@
+function rectangleCalculatorArea() {
+    // Rectangle width Input
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    
+    const rectangleWidth = parseFloat(rectangleWidthText)
+    
+
+    // rectangle Length Input
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const rectangleLength = parseFloat(rectangleLengthText);
+    
+
+    // calculate rectangle area
+    const rectangleArea = rectangleLength * rectangleWidth;
+    console.log(rectangleArea);
+
+    // Display rectangle Area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = rectangleArea;
+}
